25487fae514cfb481f6d894e1c276609f34cd324
Vadim Ermakov <vadim22.barnaul@gmail.com>
2022-04-03 20:15:25 +0300

    Create config files

---
+++ inc/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+const PROJECT_ROOT = __DIR__ . "/../";
+
+require_once PROJECT_ROOT . "/inc/config.php";
+require_once PROJECT_ROOT . "/model/Database.php";
+require_once PROJECT_ROOT . "/model/NumberModel.php";
+require_once PROJECT_ROOT . "/controller/api/BaseController.php";
+require_once PROJECT_ROOT . "/controller/api/NumberController.php";
---
+++ inc/config.php
@@ -0,0 +1,6 @@
+<?php
+
+const DB_HOST = "localhost";
+const DB_USERNAME = null;
+const DB_PASSWORD = null;
+const DB_NAME = "phonebook.sqlite3";
