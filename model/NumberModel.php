7ef52fe2ae6e51409da380633c0c3b5f15f85012
Vadim Ermakov <vadim22.barnaul@gmail.com>
2022-04-04 16:40:41 +0300

    Implement getCountryCode()

--- model/NumberModel.php
+++ model/NumberModel.php
@@ -10,19 +10,79 @@ class NumberModel extends Database
     /**
      * Get country code for a given phone number.
      * Available countries are:
-     * - RU (Russia)
-     * - US (USA)
-     * - CH (China)
-     * - MX (Mexico)
+     * - `+7` - RU (Russia)
+     * - `+1` - US (USA)
+     * - `+86` - CH (China)
+     * - `+52`, `+1905` - MX (Mexico)
      *
-     * // TODO: errors, international numbers, etc.
+     * International numbers must start with `+`.
+     * Otherwise, the number is considered russian.
      *
      * @param string $phoneNumber
      * @return string|false
      */
     public function getCountryCode(string $phoneNumber)
     {
-        // TODO
-        return "TEST_t3st";
+        // remove unnecessary characters
+        str_replace(['(', ')', '-', ' '], '', $phoneNumber);
+
+        // phone number must contain digits and `+` only
+        if (!preg_match('/\+?[0-9]+/', $phoneNumber))
+        {
+            return false;
+        }
+
+        $strRu = '+7';
+        $strUs = '+1';
+        $strCh = '+86';
+        $strMx1 = '+52';
+        $strMx2 = '+1905';
+
+        // international number
+        if ($this->strStartsWith($phoneNumber, '+'))
+        {
+            if ($this->strStartsWith($phoneNumber, $strRu))
+            {
+                return 'RU';
+            }
+
+            if ($this->strStartsWith($phoneNumber, $strUs))
+            {
+                if ($this->strStartsWith($phoneNumber, $strMx2))
+                    return 'MX';
+                else
+                    return 'US';
+            }
+
+            if ($this->strStartsWith($phoneNumber, $strCh))
+            {
+                return 'CH';
+            }
+
+            if ($this->strStartsWith($phoneNumber, $strMx1))
+            {
+                return 'MX';
+            }
+
+        }
+        else
+        {
+            return 'RU';
+        }
+
+        // unknown number code or format
+        return false;
+    }
+
+    /**
+     * Check if string starts with a specific pattern.
+     *
+     * @param string $subject
+     * @param string $pattern
+     * @return bool
+     */
+    private function strStartsWith(string $subject, string $pattern): bool
+    {
+        return substr($subject, 0, strlen($pattern)) === $pattern;
     }
 }
