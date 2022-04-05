fe654f7ce566590e152eced0c2bcd2432fc31bcb
Vadim Ermakov <vadim22.barnaul@gmail.com>
2022-04-05 18:59:41 +0300

    Add ReviewModel First method - submitReview()

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -5,5 +5,6 @@ const PROJECT_ROOT = __DIR__ . "/../";
 require_once PROJECT_ROOT . "/inc/config.php";
 require_once PROJECT_ROOT . "/model/Database.php";
 require_once PROJECT_ROOT . "/model/NumberModel.php";
+require_once PROJECT_ROOT . "/model/ReviewModel.php";
 require_once PROJECT_ROOT . "/controller/api/BaseController.php";
 require_once PROJECT_ROOT . "/controller/api/NumberController.php";
---
+++ model/ReviewModel.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Implementations for everything which can be done with reviews.
+ */
+class ReviewModel extends Database
+{
+    // TODO: upvote and downvote review
+
+    /**
+     * Submit a review for a given phone number.
+     * NOTE: the number for a review must be present in the database.
+     *
+     * @param string $phoneNumber
+     * @param string $reviewText
+     * @param string $author
+     * @return string|false
+     */
+    public function submitReview(string $phoneNumber, string $reviewText, string $author = 'anonymous')
+    {
+        // remove unnecessary characters
+        $phoneNumber = str_replace(['(', ')', '-', ' '], '', $phoneNumber);
+
+        // phone number must contain digits and `+` only
+        if (!preg_match('/\+?[0-9]+/', $phoneNumber))
+        {
+            return false;
+        }
+
+        // get id of provided number
+        $countResult = $this->executeStatement(
+            <<<SQL
+                SELECT id, COUNT(*) AS count
+                FROM phones
+                WHERE number = :phone
+                SQL,
+            [
+                ':phone' => $phoneNumber
+            ]
+        )->fetch();
+
+//        // number is not inserted - need to insert and get its id first
+//        if ($countResult['id'] === null)
+//        {
+//            $this->executeStatement(
+//                <<<SQL
+//                    INSERT INTO phones (number)
+//                    VALUES (:phone)
+//                    SQL,
+//                [
+//                    ':phone' => $phoneNumber
+//                ]
+//            );
+//
+//            $countResult['id'] = $this->executeStatement(
+//                <<<SQL
+//                    SELECT id
+//                    FROM phones
+//                    WHERE number = :phone
+//                    SQL,
+//                [
+//                    ':phone' => $phoneNumber
+//                ]
+//            )->fetch()['id'];
+//        }
+
+        $this->executeStatement(
+            <<<SQL
+                INSERT INTO reviews (num_id, body, author)
+                VALUES (:numId, :body, :author)
+                SQL,
+            [
+                ':numId' => $countResult['id'],
+                ':body' => $reviewText,
+                ':author' => $author
+            ]
+        );
+
+        // insert successful - return inserted id
+        return $countResult['id'];
+    }
+}
