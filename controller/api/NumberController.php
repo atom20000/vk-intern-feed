2ef030a73188460137913f31c54657c12bfc993e
Vadim Ermakov <vadim22.barnaul@gmail.com>
2022-04-06 18:24:00 +0300

    Add search_phonesAction() endpoint

--- controller/api/NumberController.php
+++ controller/api/NumberController.php
@@ -48,8 +48,86 @@ class NumberController extends BaseController
                 else
                 {
                     $response['description'] = 'Incorrect phone format or unavailable country';
-                    $headers[] = 'HTTP/1.1 400 Bad Request';
+                    $responseHeaders[] = 'HTTP/1.1 400 Bad Request';
                 }
+            }
+            else
+            {
+                $response['description'] = 'The phone_number parameter must be provided';
+                $responseHeaders[] = 'HTTP/1.1 400 Bad Request';
+            }
+        }
+        else
+        {
+            $response['description'] = 'HTTP method is not supported';
+            $responseHeaders[] = 'HTTP/1.1 400 Bad Request';
+        }
+
+        $responseHeaders[] = 'Content-Type: application/json';
+
+        $this->sendOutput(json_encode($response), $responseHeaders);
+    }
+
+    /**
+     * Endpoint `/phone/search_phones` - search for phone numbers
+     * starting with a given pattern.
+     */
+    public function search_phonesAction()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $params = $this->getQueryStringParams();
+        $response = [
+            'error' => true
+        ];
+        $responseHeaders = [];
+
+        if (strtoupper($method) === 'GET')
+        {
+            if (isset($params['phone_pattern']) && $params['phone_pattern'])
+            {
+                $numberModel = new NumberModel();
+                $reviewModel = new ReviewModel();
+                $matches = null;
+
+                try
+                {
+                    $matches = $numberModel->matchPhone($params['phone_pattern']);
+                }
+                catch (UnexpectedValueException $ex)
+                {
+                    $response['description'] = 'Internal Server Error';
+                    $responseHeaders[] = 'HTTP/1.1 500 Internal Server Error';
+                    $responseHeaders[] = 'Content-Type: application/json';
+
+                    $this->sendOutput($response, $responseHeaders);
+                }
+
+                if ($matches)
+                {
+                    $response['error'] = false;
+                    $response['result'] = [
+                        'phone_pattern' => $params['phone_pattern'],
+                        'matches' => []
+                    ];
+
+                    foreach ($matches as $m)
+                    {
+                        $response['result']['matches'][] = [
+                            'phone_number' => $m['number'],
+                            'reviews_count' => count($reviewModel->getReviews($m['id']))
+                        ];
+                    }
+                }
+                else
+                {
+                    $response['description'] = 'Incorrect phone search pattern, no phones found';
+                    $responseHeaders[] = 'HTTP/1.1 400 Bad Request';
+                }
+            }
+            else
+            {
+                $response['description'] = 'The phone_pattern parameter must be provided';
+                $responseHeaders[] = 'HTTP/1.1 400 Bad Request';
             }
         }
         else
